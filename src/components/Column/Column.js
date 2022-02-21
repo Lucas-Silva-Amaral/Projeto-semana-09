bc8959e45e092678003d893bd495e7d67db5498c
Tiago Santos <tiago.silva.qa@gmail.com>
2022-02-21 17:40:47 -0300

    Adicionado tarefas dentro das colunas

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,6 +4,7 @@ import toDo from "./img/To-Do.png";
 import inProgress from "./img/In-Progress.png";
 import review from "./img/Review.png";
 import done from "./img/Done.png";
+import Article from "../Article/Article";
 
 const Column = (props) => {
   const { children, status } = props;
@@ -12,19 +13,22 @@ const Column = (props) => {
     <section>
       <div className={`top-column status-${status}-top`}></div>
       <div className={`column status-${status}`}>
-        <img
-          className="img"
-          src={
-            status == "to-do"
-              ? toDo
-              : status == "in-progress"
-              ? inProgress
-              : status == "review"
-              ? review
-              : done
-          }
-        />
-        <p>{children}</p>
+        <div>
+          <img
+            className="img"
+            src={
+              status === "to-do"
+                ? toDo
+                : status === "in-progress"
+                ? inProgress
+                : status === "review"
+                ? review
+                : done
+            }
+          />
+          <p>{children}</p>
+        </div>
+        <Article />
       </div>
     </section>
   );
